72024ec1741ea6e1776c891a491cd5d7b9b5e261
Dan Zaratsian <dtz001@gmail.com>
2018-11-19 08:00:58 -0500

    Update gcp_bigquery.py

--- gcp_bigquery.py
+++ gcp_bigquery.py
@@ -15,28 +15,72 @@ GENERAL NOTES
 '''
 '''
 
-Renaming Datasets
-Currently, you cannot change the name of an existing dataset, and you cannot copy a dataset and give it a new name.
-If you need to change the dataset name, follow these steps to recreate the dataset:
-    1. Create a new dataset and specify the new name.
-    2. Copy the tables from the old dataset to the new one.
-    3. Recreate the views in the new dataset.
-    4. Delete the old dataset to avoid additional storage costs.
 
-Copying Datasets
-Currently, you cannot copy a dataset. Instead follow these steps to recreate the dataset:
-    1. Create a new dataset (with a unique name per project)
-    2. Copy the tables from the old dataset to the new one.
-    3. Recreate the views in the new dataset.
-    4. Delete the old dataset to avoid additional storage costs.
+BigQuery Datasets
+
+    Renaming Datasets
+    Currently, you cannot change the name of an existing dataset, and you cannot copy a dataset and give it a new name.
+    If you need to change the dataset name, follow these steps to recreate the dataset:
+        1. Create a new dataset and specify the new name.
+        2. Copy the tables from the old dataset to the new one.
+        3. Recreate the views in the new dataset.
+        4. Delete the old dataset to avoid additional storage costs.
+    
+    Copying Datasets
+    Currently, you cannot copy a dataset. Instead follow these steps to recreate the dataset:
+        1. Create a new dataset (with a unique name per project)
+        2. Copy the tables from the old dataset to the new one.
+        3. Recreate the views in the new dataset.
+        4. Delete the old dataset to avoid additional storage costs.
+    
+    
 
 
 
-To Review:
+
+BigQuery Tables
+
+    You can specify the schema of a table when it is created,
+    or you can create a table without a schema and declare the schema in the query job or load job that first populates it with data.
+
+    BigQuery supports the following table types:
+        - Native tables
+        - External tables (support for BigTable, Cloud Storage, Google Drive)
+        - Views: Virtual tables defined by a SQL query
+    
+    Table limitations:
+        - Table names must be unique per dataset.
+        - Web UI supports copying only one table at a time
+        - When copying tables, the destination dataset must reside in the same location as the table being copied.
+        - When copying multiple source tables to a destination table using the CLI or API, all source tables must have identical schemas.
+        - You can only delete one table at a time.
+        - When exporting table data, the only supported destination is Google Cloud Storage.
+        - As you approach 50,000 or more tables in a dataset, enumerating them becomes slower.
+    
+    Controlling access to tables
+        - You cannot assign access controls directly to tables.
+        - You can control table access by configuring access controls at the dataset level or at the project level.
+
+    Renaming a Table
+    Currently, you cannot change the name of an existing table.
+    If you need to change the table name, follow the steps to copy the table.
+    When you specify the destination table in the copy operation, use the new table name.
+    
+    Copying a table
+    You can copy a table by using the BigQuery web UI, the command-line tool's bq cp command,
+    or by calling the jobs.insert API method and configuring a copy job.
+
+
+
+
+
+
+To Do:
     [ ] How-to Guides: Running and Managing Jobs
     [X] Can I secure access to only one table within a Dataset? No, see here https://cloud.google.com/bigquery/docs/datasets
-    [ ] Great content in Working with Datasets: Create and Using Datasets
-
+    [ ] Great content in Working with Datasets: Create and Using Datasets - Review before exam.
+    [ ] Skipped working with Table Schemas
+    [ ] Can I copy a table from dataset1 to dataset2 (assumes that ds1 and ds2 are in the same region)
 
 
 '''
@@ -53,7 +97,7 @@ import os
 
 from google.cloud import bigquery
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/dzaratsian/zproject201807-d5eb54b6371e.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/dzaratsian/key.json'
 
 
 ####################################################################################################
@@ -367,7 +411,245 @@ def bq_delete_dataset(dataset_id):
 
 
 
-# Create BigQuery Table
+def bq_table_metadata(dataset_id, table_id):
+    '''
+        List all metadata for a BigQuery Table
+        
+        USAGE:
+        bq_table_metadata('zdataset','airlines')
+        
+        Required Permissions:
+        To get information about tables, you must be assigned the READER role on the dataset,
+        or you must be assigned a project-level IAM role that includes bigquery.tables.get permissions.
+       
+    '''
+    try:
+        client      = bigquery.Client()
+        dataset_ref = client.dataset(dataset_id)
+        table_ref   = dataset_ref.table(table_id)
+        table       = client.get_table(table_ref)
+        
+        # View table properties
+        print('Table:                {}'.format(table.table_id))
+        print('Description:          {}'.format(table.description))
+        print('Dataset:              {}'.format(table.dataset_id))
+        print('Path:                 {}'.format(table.path))
+        print('Created:              {}'.format(table.created))
+        print('Expires(ms):          {}'.format(table.created))
+        print('Location:             {}'.format(table.location))
+        print('Table Type:           {}'.format(table.table_type))
+        print('Num Rows:             {}'.format(table.num_rows))
+        print('Num Bytes:            {}'.format(table.num_bytes))
+        print('Partition Type:       {}'.format(table.partitioning_type))
+        print('Partition Expiration: {}'.format(table.partition_expiration))
+        print('Time Partitioning:    {}'.format(table.time_partitioning))
+        print('Schema:')
+        for column in table.schema:
+            print('\t{}'.format(column))
+    
+    except Exception as e:
+        print('[ ERROR ] {}'.format(e))
+
+
+
+
+def bq_list_tables(dataset_id):
+    '''
+        List tables within a BigQuery Dataset
+    
+        USAGE:
+        bq_list_tables('zdataset')
+        
+        Required Permissions:
+        To list tables in a dataset, you must be assigned the READER role on the dataset,
+        or you must be assigned a project-level IAM role that includes bigquery.tables.list permissions.
+        
+    '''
+    try:
+        client      = bigquery.Client()
+        dataset_ref = client.dataset(dataset_id)
+        tables      = list(client.list_tables(dataset_ref))
+        
+        for table in tables:
+            print('Table:               {}'.format(table.table_id))
+            print('Dataset:             {}'.format(table.dataset_id))
+            print('Project:             {}'.format(table.project))
+            print('Table Type:          {}'.format(table.table_type))
+            print('Time Partitioning:   {}'.format(table.time_partitioning))
+            print('')
+        
+        print('Total Number of Table: {}'.format(len(tables)))
+    
+    except Exception as e:
+        print('[ ERROR ] {}'.format(e))
+
+
+
+
+
+
+def bq_update_table_metadata(dataset_id, table_id, new_description=None, new_table_expiration=None):
+    '''
+        Update metadata / properties for a BigQuery Table
+        
+        USAGE:
+        bq_update_table_metadata('demo', 'ztable1', new_description='my new desc', new_table_expiration='2018-11-30 08:00:00')
+        
+        Required Permissions:
+        To update table properties, you must have WRITER access at the dataset level,
+        or you must be assigned a project-level IAM role that includes requires bigquery.tables.update permissions.
+        The following predefined, project-level IAM roles include bigquery.tables.update permissions:
+            bigquery.dataEditor
+            bigquery.dataOwner
+            bigquery.admin
+        
+    '''
+    try:
+        client    = bigquery.Client()
+        table_ref = client.dataset(dataset_id).table(table_id)
+        table     = client.get_table(table_ref)
+        
+        if new_description != None:
+            table.description = new_description
+            table = client.update_table(table, ['description'])
+        
+        '''
+        You can set a default table expiration time at the dataset level,
+        or you can set a table's expiration time when the table is created.
+        A table's expiration time is often referred to as "time to live" or TTL.
+        
+        When you update a table's expiration time, you must calculate the expiration based
+        on the table's creation time. For example, if your table was created on January 3, 2018,
+        and today is January 5, if you set the expiration time at 1 day using the BigQuery web UI,
+        the expiration time is considered "in the past" and the table is deleted immediately.
+        '''
+        if new_table_expiration != None:
+            table.expires = datetime.datetime.strptime(new_table_expiration, '%Y-%m-%d %H:%M:%S')
+            table = client.update_table(table, ['expires'])
+    
+    except Exception as e:
+        print('[ ERROR ] {}'.format(e))
+
+
+
+
+
+def bq_create_table_empty(dataset_id, table_id):
+    '''
+        Creates an empty BigQuery Table
+        
+        USAGE:
+        bq_create_table_empty('ztest1', 'ztable1')
+        
+        Required Permissions:
+        To create a table, you must have WRITER access at the dataset level,
+        or you must be assigned a project-level IAM role that includes bigquery.tables.create permissions.
+        The following predefined, project-level IAM roles include bigquery.tables.create permissions:
+            bigquery.dataEditor
+            bigquery.dataOwner
+            bigquery.admin
+    
+    '''
+    try:
+        client      = bigquery.Client()
+        dataset_ref = client.dataset(dataset_id)
+        
+        schema = [
+            bigquery.SchemaField('full_name', 'STRING', mode='REQUIRED'),
+            bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+        ]
+        
+        table_ref = dataset_ref.table(table_id)
+        table     = bigquery.Table(table_ref, schema=schema)
+        table     = client.create_table(table)
+        
+        assert table.table_id == table_id
+    except Exception as e:
+        print('[ ERROR] {}'.format(e))
+
+
+
+
+def bq_create_table_from_query(dataset_id, table_id, sql_query, location='US'):
+    '''
+        Create a table from a query result, write the results to a destination table.
+        
+        USAGE:
+        bq_create_table_from_query('ztest1', 'ztable2', """SELECT corpus FROM `bigquery-public-data.samples.shakespeare`GROUP BY corpus;""", 'US')
+    
+    '''
+    try:
+        client     = bigquery.Client()
+        job_config = bigquery.QueryJobConfig()
+        # Set the destination table
+        table_ref = client.dataset(dataset_id).table(table_id)
+        job_config.destination = table_ref
+        
+        # Example Query
+        '''
+        sql_query = """
+            SELECT corpus
+            FROM `bigquery-public-data.samples.shakespeare`
+            GROUP BY corpus;
+        """
+        '''
+        
+        # Start the query, passing in the extra configuration.
+        
+        query_job = client.query(
+            sql_query,
+            # Location must match that of the dataset(s) referenced
+            # in the query and of the destination table.
+            location='US',
+            job_config=job_config)
+        
+        query_job.result()  # Waits for the query to finish
+        print('Query results loaded to table {}'.format(table_ref.path))
+    except Exception as e:
+        print('[ ERROR] {}'.format(e))
+
+        
+        
+
+def bq_copy_table(project_id, source_dataset, source_table, dest_dataset, dest_table):
+    '''
+        Copies a BigQuery Table
+        
+        USAGE:
+        
+        
+        NOTE: Multiple source files can be copied to a destination table by using the CLI or API.
+        
+        Required Permissions
+        At the dataset level, copying a table requires READER access to the source dataset that contains the table being copied,
+        and WRITER access to the destination dataset.
+        
+        Limitations on copying Tables:
+            - Table copies are subject to BigQuery's quota policies on copy jobs.
+            - When copying tables, the destination dataset must reside in the same location as the dataset containing the table being copied.
+            - Copying multiple source tables into a destination table is not supported by the web UI.
+            - IDENTICAL SCHEMAS - When copying multiple source tables to a destination table using the CLI or API, all source tables must have identical schemas
+    
+    '''
+client = bigquery.Client()
+
+source_dataset   = client.dataset(source_dataset, project=project_id)
+source_table_ref = source_dataset.table(source_table)
+
+dest_table_ref   = client.dataset(dest_dataset).table(dest_table)
+
+job = client.copy_table(
+    source_table_ref,
+    dest_table_ref,
+    # Location must match that of the source and destination tables.
+    location='US')
+
+job.result()  # Waits for job to complete.
+
+assert job.state == 'DONE'
+
+
+
 
 
 
@@ -395,8 +677,6 @@ def bq_delete_dataset(dataset_id):
 
 
 # Export Data
-
-# Copy Data
 
 
 
